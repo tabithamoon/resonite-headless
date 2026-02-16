61735bee9ed1798f5d088de35b9b3ce9aa846c35
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-02-15 22:44:00 -0300

    Add ResoniteModLoader to startup script

--- src/start_resonite.sh
+++ src/start_resonite.sh
@@ -2,7 +2,7 @@
 
 if [ -f ${STEAMAPPDIR}/Headless/Resonite.dll ]; then
 	echo 'Resonite.dll is in the new (permanent) location, running...'
-	exec ${STEAMAPPDIR}/dotnet-runtime/dotnet ${STEAMAPPDIR}/Headless/Resonite.dll -HeadlessConfig /Config/Config.json -Logs /Logs
+	exec ${STEAMAPPDIR}/dotnet-runtime/dotnet ${STEAMAPPDIR}/Headless/Resonite.dll -HeadlessConfig /Config/Config.json -Logs /Logs -LoadAssembly Libraries/ResoniteModLoader.dll
 else
 	echo 'Resonite.dll not found, weird!'
 	sleep 10
