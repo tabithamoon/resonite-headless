bd096bdf9cb1105aec0c88081eb9738f2a840ebe
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-02-15 23:23:56 -0300

    Switch to using curl instead of wget [skip ci]
    wget was generating duplicates.

--- src/setup_resonite.sh
+++ src/setup_resonite.sh
@@ -22,7 +22,16 @@ find /Logs -type f -name '*.log' -atime +30 -delete
 mkdir -p Headless/Migrations
 
 mkdir -p ${STEAMAPPDIR}/Headless/Libraries ${STEAMAPPDIR}/Headless/rml_libs ${STEAMAPPDIR}/Headless/rml_mods ${STEAMAPPDIR}/Headless/rml_config
-cd ${STEAMAPPDIR}/Headless/Libraries && curl -s https://api.github.com/repos/resonite-modding-group/ResoniteModLoader/releases/latest | grep "browser_download_url.*ResoniteModLoader.dll" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-cd ${STEAMAPPDIR}/Headless/rml_libs && curl -s https://api.github.com/repos/resonite-modding-group/ResoniteModLoader/releases/latest | grep "browser_download_url.*0Harmony.dll" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+cd ${STEAMAPPDIR}/Headless/Libraries && \
+	curl -s https://api.github.com/repos/resonite-modding-group/ResoniteModLoader/releases/latest | \
+	grep "browser_download_url.*ResoniteModLoader.dll" | \
+	cut -d : -f 2,3 | tr -d \" | \
+	xargs curl -s -L --output ResoniteModLoader.dll
+	
+cd ${STEAMAPPDIR}/Headless/rml_libs && \
+	curl -s https://api.github.com/repos/resonite-modding-group/ResoniteModLoader/releases/latest | \
+	grep "browser_download_url.*0Harmony.dll" | \
+	cut -d : -f 2,3 | tr -d \" | \
+	xargs curl -s -L --output 0Harmony.dll
 
 exec $*
