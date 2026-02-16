4488c756e236696bdf5880e7ab0991ee12896bb5
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-02-15 22:41:42 -0300

    Add ResoniteModLoader installer to setup script

--- src/setup_resonite.sh
+++ src/setup_resonite.sh
@@ -16,8 +16,13 @@ bash "${STEAMCMDDIR}/steamcmd.sh" \
 chmod +x ${STEAMAPPDIR}/dotnet-install.sh
 ${STEAMAPPDIR}/dotnet-install.sh --channel ${DOTNETVERSION} --runtime dotnet --install-dir ${STEAMAPPDIR}/dotnet-runtime
 
-find ${STEAMAPPDIR}/Data/Assets -type f -atime +7 -delete
-find ${STEAMAPPDIR}/Data/Cache -type f -atime +7 -delete
+find ${STEAMAPPDIR}/Headless/Data/Assets -type f -atime +7 -delete
+find ${STEAMAPPDIR}/Headless/Cache -type f -atime +7 -delete
 find /Logs -type f -name *.log -atime +30 -delete
 mkdir -p Headless/Migrations
+
+mkdir -p ${STEAMAPPDIR}/Headless/Libraries ${STEAMAPPDIR}/Headless/rml_libs ${STEAMAPPDIR}/Headless/rml_mods ${STEAMAPPDIR}/Headless/rml_config
+cd ${STEAMAPPDIR}/Headless/Libraries && curl -s https://api.github.com/repos/resonite-modding-group/ResoniteModLoader/releases/latest | grep "browser_download_url.*ResoniteModLoader.dll" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+cd ${STEAMAPPDIR}/Headless/rml_libs && curl -s https://api.github.com/repos/resonite-modding-group/ResoniteModLoader/releases/latest | grep "browser_download_url.*0Harmony.dll" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+
 exec $*
